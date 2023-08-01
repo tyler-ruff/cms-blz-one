4cbfa52a2f2046409990942cea04b21e0fbcabb0
Tyler Ruff <hello@blazed.space>
2023-08-01 16:34:08 -0400

    test

---
+++ collections/catalog.tsx
@@ -0,0 +1,48 @@
+import { buildCollection, buildProperty, EntityReference } from "firecms";
+import { localeCollection } from "./locales.tsx";
+
+export type Catalog = {
+    title: string,
+    description: string,
+    download: string,
+    type: string
+}
+
+export const catalogCollection = buildCollection<Catalog>({
+    name: "Publications",
+    singularName: "Publication",
+    path: "catalog",
+    icon: "LocalGroceryStore",
+    group: "Publishing",
+    permissions: ({ authController, user }) => ({
+        read: true,
+        edit: true,
+        create: true,
+        delete: true
+    }),
+    subcollections: [
+        localeCollection
+    ],
+    properties: {
+        title: {
+            name: "Title",
+            validation: { required: true },
+            dataType: "string"
+        },
+        description: {
+            name: "Description",
+            validation: { required: true },
+            dataType: "string"
+        },
+        download: {
+            name: "Download Link",
+            validation: { required: true },
+            dataType: "string"
+        },
+        type: {
+            name: "Type",
+            validation: { required: true },
+            dataType: "string"
+        }
+    }
+});
---
+++ firebase-config.ts
@@ -0,0 +1,11 @@
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDJNpSgZqhn1_ipvk54wsU11UIE1BoYIfc",
+    authDomain: "blz-one-9e383.firebaseapp.com",
+    databaseURL: "https://blz-one-9e383-default-rtdb.firebaseio.com",
+    projectId: "blz-one-9e383",
+    storageBucket: "blz-one-9e383.appspot.com",
+    messagingSenderId: "817319905653",
+    appId: "1:817319905653:web:634500e5cb0dcbce8bab3c",
+    measurementId: "G-96ENSBPH6N"
+};
